51acd8943c5617f12609e45c631fa27249e62278
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-03-04 15:11:39 -0500

    prep four

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,13 +1,27 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
-        do
+        bool entry = true;
+        string response;
+        List<int> num = new List<int>();
+        while (entry == true)
         {
-            Console.WriteLine("")
+            Console.WriteLine("Enter an integer: ");
+            response = Console.ReadLine();
+            int compare = int.Parse(response);
+            if (compare == 0)
+            {
+                entry = false;
+            }
+            else
+            {
+                num.Add(compare);
+            }
         }
     }
 }
