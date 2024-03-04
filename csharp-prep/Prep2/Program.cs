1e091e2e59535821a1fbcfd7f2eb928b1a4aedb9
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-03-03 23:28:15 -0500

    prep two

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade %? (Integers only)");
+        string input = Console.ReadLine();
+        int percent = int.Parse(input);
+        string grade = " ";
+        if (percent >= 90)
+        {
+            grade = ("A");
+        }
+        else if (percent >= 80)
+        {
+            grade =("B");
+        }
+        else if (percent >= 70)
+        {
+            grade = ("C");
+        }
+        else if (percent >= 60)
+        {
+            grade = ("D");
+        }
+        else
+        {
+            grade = "F";
+        }
+        if (grade == "A" || grade == "B" || grade == "C")
+        {
+            Console.WriteLine($"You passed with a {grade}.");
+        }
+        else
+        {
+            Console.WriteLine($"You don't pass with a {grade}.");
+        }
     }
 }
