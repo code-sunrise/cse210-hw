827984678791f5009dddd933141425e67772ce53
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-03-04 14:23:34 -0500

    prep three

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        Console.WriteLine($"The Magic Number is {magicNumber}");
+        bool win = false;
+        while (win == false)
+        {
+            Console.WriteLine("Enter your guess!");
+            string guess = Console.ReadLine();
+            int compare = int.Parse(guess);
+            if (compare == magicNumber)
+            {
+                Console.WriteLine($"That's correct, the magic number is {magicNumber}!");
+                win = true;
+            }
+            else if (compare > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else if (compare < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+                break;
+            }
+        }
+        Console.WriteLine("Thanks for playing!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        do
+        {
+            Console.WriteLine("")
+        }
     }
 }
