5472d78087e6cc6476d399c61127d5a31ca130b8
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-03-04 19:15:06 -0500

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
+        string name = AskUserName();
+        int num = FavoriteNumber();
+        int numSquared = SquareNumber(num);
+        DisplayResults(name, numSquared);
     }
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string AskUserName()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        static int FavoriteNumber()
+        {
+            Console.WriteLine("What is your favorite number?");
+            string favoriteString = Console.ReadLine();
+            int favoriteNumber = int.Parse(favoriteString);
+            return favoriteNumber;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        static void DisplayResults(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}!");
+        }
 }
